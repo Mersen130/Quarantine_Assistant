540b75af70574054223d7cf38c490e6370c759d6
Mersen130 <mersenyip0130@outlook.com>
2020-06-14 19:07:26 -0400

    add sidebar

---
+++ quarantine/src/SideNavBar/sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../App.css';
+import './sidebar.css'
+
+class Sidebar extends React.Component {
+
+  state = {title: "Dashboard"}
+
+  render(){
+    return (
+      <div>
+        <div id="mySidebar" class="sidebar">
+            <a href="javascript:void(0)" class="closebtn" onClick={this.closeSide}>&times;</a>
+            {/* TODO: change hrefs to appropriate links */}
+            {/*Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>*/}
+            <a><img id="profilePhoto" src={require("./lib/sampleprofile.png")}/></a>
+            <p id="profileName" >Annonymous</p>
+            <a onClick={this.changeNavbarTitle} href="#"><img src={require("./lib/dashboard.png")}/>        Dashboard</a>
+            <a onClick={this.changeNavbarTitle} href="#"><img src={require("./lib/qa.png")}/>        Q&A</a>
+            <a onClick={this.changeNavbarTitle} href="#"><img src={require("./lib/activity.png")}/>        Activities</a>
+            <a onClick={this.changeNavbarTitle} href="#"><img src={require("./lib/nearby.png")}/>        People nearby</a>
+            <a href="#" id="logout">log out</a>
+        </div>
+
+        <div id="main">
+    <button class="openbtn" onClick={this.openSide}>&#9776; {this.state.title}</button>
+        </div>
+
+
+      </div>
+    );
+  }
+
+  changeNavbarTitle = (e) => {
+    this.setState({title: e.target.innerText});
+  }
+
+  openSide = () => {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  closeSide = () => {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+}
+
+export default Sidebar;
