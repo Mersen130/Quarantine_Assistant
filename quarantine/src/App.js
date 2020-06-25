6dd28df04c777995749671945560502cd6561af3
YifeiGao <yiffygao@gmail.com>
2020-06-25 14:54:56 -0400

    SignIn&Up without backgroud color and logo pic

--- quarantine/src/App.js
+++ quarantine/src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './SideNavBar/sidebar.js'
+import SignIn from './react-components/SignIn/signIn.js'
+import SignUp from './react-components/SignUp/signUp.js'
+import Reset from './react-components/ResetPwd/resetPwd.js'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <Sidebar/>
-      </div>
+      <React.Fragment>
+      	<Router>
+      		<Switch>
+      			<Route exact path="/" component={SignIn} />
+      			<Route path="/SignUp" component={SignUp} />
+      			<Route path="/Reset" component={Reset} />
+      		</Switch>
+      	</Router>
+      </React.Fragment>
     );
   }
 }
 
 export default App;
+ 
---
+++ quarantine/src/react-components/SignUp/signUp.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Form, Image, Row, Col,FormGroup, Label, Nav} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SignUp.css';
+
+
+class SignUp extends React.Component{
+
+	render(){
+		return(
+		<div>
+		<Form className = "signUpForm">
+			<Image src="./../../media/calendar.png/40x40" roundedCircle/>
+			<h2 calssName = "signUp">Sign Up
+			</h2>
+  			<Form.Row>
+    			<Form.Group as={Col} controlId="formGridFirstName">
+      				<Form.Control type="firstName" placeholder="First Name*" />
+    			</Form.Group>
+				<Form.Group as={Col} controlId="formGridLastName">
+      				<Form.Control type="lastName" placeholder="Last Name*" />
+    			</Form.Group>
+  			</Form.Row>
+			<Form.Group controlId="formGridEmail">
+    			<Form.Control placeholder="Email Address*" />
+  			</Form.Group>
+			 <Form.Group controlId="formGridPassword">
+    			<Form.Control placeholder="Password*" />
+  			</Form.Group>
+  			<Form.Group controlId = "formGridDocId">
+  				<Form.Control placeholder = "Doctor certificate No *" />
+  			</Form.Group>
+  			<Form.Group controlId="formGridCheckbox">
+    			<Form.Check type="checkbox" label="I want to recieve notifications via email" />
+  			</Form.Group>
+			<Button variant="primary" type="submit">Sign Up</Button>
+			<LinkContainer to="/">
+        <Button variant = "link">Already have an account? Sign In</Button>
+ 		 </LinkContainer>
+    </Form>
+		</div>
+			);
+
+	}
+}
+export default SignUp;
