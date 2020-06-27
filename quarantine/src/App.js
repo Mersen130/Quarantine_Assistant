b27e65141bfb7dff0ba91bd06f8639ae7a1b3d14
Mersen130 <mersenyip0130@outlook.com>
2020-06-27 14:30:41 -0400

    bug fix

--- quarantine/src/App.js
+++ quarantine/src/App.js
@@ -4,7 +4,6 @@ import Sidebar from './SideNavBar/sidebar.js'
 import SignIn from './react-components/SignIn/signIn.js'
 import SignUp from './react-components/SignUp/signUp.js'
 import Reset from './react-components/ResetPwd/resetPwd.js'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import QA from './QA/qa.js';
 import Questionnaire from './questionnaire/questionnaire.js';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
@@ -18,22 +17,13 @@ class App extends React.Component {
       			<Route exact path="/" component={SignIn} />
       			<Route path="/SignUp" component={SignUp} />
       			<Route path="/Reset" component={Reset} />
+            <Route path="/questionnaire/" component={Questionnaire} />
+            <Route path="/qa/" component={QA} />
       		</Switch>
       	</Router>
       </React.Fragment>
     );
   }
-
-      <div className="App">
-          <Router>
-            <Switch>
-              <Route path="/questionnaire/" component={Questionnaire} />
-              <Route path="/qa/" component={QA} />
-            </Switch>
-          </Router>
-        </div>
-        );
-    }
 }
 
 export default App;
