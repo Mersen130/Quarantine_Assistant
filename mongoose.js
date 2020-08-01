7c251b3bcf5d0d791fd3780309f9d2637ab8c715
Mersen130 <mersenyip0130@outlook.com>
2020-08-01 23:46:36 +0900

    set up server, mongoose connection

---
+++ mongoose.js
@@ -0,0 +1,12 @@
+'use strict';
+// ASK QIXIN UPON CHANGING THIS FILE
+const mongoose = require('mongoose');
+
+// ASK QIXIN UPON CHANGING THIS FILE
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/QuarantineAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+// ASK QIXIN UPON CHANGING THIS FILE
+module.exports = { mongoose }
+
+// after deployed, in terminal: 
+// heroku config:set MONGODB_URI='mongodb+srv://qixin:mymongo@cluster0.htkva.mongodb.net/QuarantineAPI?retryWrites=true&w=majority'
--- quarantine/server.js
+++ quarantine/server.js
@@ -3,37 +3,37 @@
 const log = console.log
 log('Express server')
 
-const express = require('express')
 const path = require('path')
 
+// Express
+const express = require('express')
 const app = express();
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json());
 
-// Setting up a static directory for the files in /pub
+// Mongo and Mongoose
+const { ObjectID } = require('mongodb')
+const { mongoose } = require('./mongoose');
+const { Post, Notification } = require('./schema');  // TODO: update this
+const { Collection } = require('mongoose');
+
+// Setting up a static directory for the files in /public
 // using Express middleware.
-// Don't put anything in /pub that you don't want the public to have access to!
-app.use(express.static(__dirname + '/public'))
+// Don't put anything in /public that you don't want the public to have access to!
+app.get("*", (req, res) => {
+    // check for page routes that we expect in the frontend to provide correct status code.
+    const goodPageRoutes = ["/", "/qa", "questionnaire", "qaadmin", "profile"];
+    if (!goodPageRoutes.includes(req.url)) {
+        // if url not in expected page routes, set status to 404.
+        res.status(404);
+    }
 
-// Let's make some express 'routes'
-// Express has something called a Router, which 
-// takes specific HTTP requests and handles them
-// based on the HTTP method and URL
-
-// Let's make a route for an HTTP GET request to the 
-// 'root' of our app (i.e. top level domain '/')
-
-app.get('/', (req, res) => {
-	// sending a string
-	//res.send('This should be the root route!')
-
-	//sending some HTML
-	res.sendFile(path.join(__dirname + '/public/index.html'));
-})
+    // send index.html
+    res.sendFile(__dirname + "/quarantine/build/index.html");
+});
 
 // will use an 'environmental variable', process.env.PORT, for deployment.
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
-})  // localhost development port 5000  (http://localhost:5000)
-   // We've bound that port to localhost to go to our express server.
-   // Must restart web server when you make changes to route handlers.
-
+})
