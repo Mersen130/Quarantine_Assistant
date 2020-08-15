de60508f5b71997188582ca25ae4c160a81fbd6d
Mersen130 <mersenyip0130@outlook.com>
2020-08-15 19:06:34 +0900

    push build
    not sure if this is required

---
+++ quarantine/build/precache-manifest.a7322f478a06d5957a19c622fcfda34b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93838e3181e4d556370139a621bb8679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe90e12d8d71bb53d6a",
+    "url": "/static/css/2.c1fedb3d.chunk.css"
+  },
+  {
+    "revision": "503bfefe5bcc35c246cb",
+    "url": "/static/css/main.3b268225.chunk.css"
+  },
+  {
+    "revision": "efe90e12d8d71bb53d6a",
+    "url": "/static/js/2.238cd87f.chunk.js"
+  },
+  {
+    "revision": "84194fe92eb6207d61083095176837ed",
+    "url": "/static/js/2.238cd87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503bfefe5bcc35c246cb",
+    "url": "/static/js/main.df588117.chunk.js"
+  },
+  {
+    "revision": "03f6a594592b587dd1ca",
+    "url": "/static/js/runtime-main.511d6b44.js"
+  },
+  {
+    "revision": "d8119ad0c9cdd6bb82535ce83e703a0a",
+    "url": "/static/media/Yoga.d8119ad0.png"
+  },
+  {
+    "revision": "f19967d4ec47f0ae1d9daffda83c0048",
+    "url": "/static/media/appLogo.f19967d4.png"
+  },
+  {
+    "revision": "80b48b36b9b7d5f68327caeeb768c0d8",
+    "url": "/static/media/bg1.80b48b36.jpeg"
+  },
+  {
+    "revision": "c1759d446670d53976cb8f99898b4e19",
+    "url": "/static/media/bg2.c1759d44.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c13c55fbb611afd6732553cf095b9752",
+    "url": "/static/media/sampleprofile.c13c55fb.png"
+  },
+  {
+    "revision": "de49561b5cb44e379a7d2ea02358f4f4",
+    "url": "/static/media/strange.de49561b.png"
+  },
+  {
+    "revision": "c13c55fbb611afd6732553cf095b9752",
+    "url": "/static/media/user1.c13c55fb.png"
+  },
+  {
+    "revision": "d6e978aaa1f0467fe0c1226593465ed0",
+    "url": "/static/media/user2.d6e978aa.png"
+  },
+  {
+    "revision": "ac5d72e4ee60ad9f134fb677abfa2aa9",
+    "url": "/static/media/user3.ac5d72e4.png"
+  }
+]);
